9b45a040f3f17aa92e7a27fd5a62ab11dfccb282
aadi308 <manish.aadi57@gmail.com>
2023-04-05 18:50:55 +0545

    py code

--- kamaji_tcp.py
+++ kamaji_tcp.py
@@ -1,104 +1,76 @@
 import subprocess
 import yaml
+import time
+import os
+
+def create_cluster(cluster_name, yaml_file_path):
+    secret_name = f"{cluster_name}-admin-kubeconfig"
+    config = f"/tmp/kubeconfig"
+    yaml_file_path= "kamaji-tenant.yaml"
+
+    with open(yaml_file_path) as f:
+        tenant_yaml = f.read().replace('<cluster_name>', cluster_name)
 
 
-# cert = "kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml"
-# subprocess.run(cert, shell=True, check=True)
-# # Add Clastix Helm repository
-# add_repo_command = ['helm', 'repo', 'add', 'clastix', 'https://clastix.github.io/charts']
-# subprocess.run(add_repo_command, check=True)
+    # Use kubectl to create the CRD
+    subprocess.run(["kubectl", "apply", "-f", "-"], input=tenant_yaml.encode())
 
-# # Update Helm repositories
-# update_repo_command = ['helm', 'repo', 'update']
-# subprocess.run(update_repo_command, check=True)
+    time.sleep(40)
+    # retrieve kubeconfig and store in /tmp/kubeconfig
+    config_detail = f'kubectl get secrets {secret_name} -o json | jq -r \'.data["admin.conf"]\' | base64 -d'
+    subprocess.run(config_detail, shell=True, check=True)
+  
+    
+    command = f'kubectl get secrets {secret_name} -o json | jq -r \'.data["admin.conf"]\' | base64 -d > {config}'
+    subprocess.run(command, shell=True, check=True)
 
-# # Install Kamaji via Helm
-# install_command = ['helm', 'install', 'kamaji', 'clastix/kamaji', '-n', 'kamaji-system', '--create-namespace']
-# subprocess.run(install_command, check=True)
-cluster_name = "tenant1"
-secret_name = f"{cluster_name}-admin-kubeconfig"
-config = f"/tmp/kubeconfig3"
-tenant = {
-    'apiVersion': 'kamaji.clastix.io/v1alpha1',
-    'kind': 'TenantControlPlane',
-    'metadata': {
-        'name': cluster_name
-    },
-    'spec': {
-        'controlPlane': {
-            'deployment': {
-                'replicas': 2,
-                'additionalMetadata': {
-                    'annotations': {
-                        'environment.clastix.io': 'tenant1',
-                        'tier.clastix.io': '0'
-                    },
-                    'labels': {
-                        'tenant.clastix.io': 'tenant1',
-                        'kind.clastix.io': 'deployment'
-                    }
-                }
-            },
-            'service': {
-                'additionalMetadata': {
-                    'annotations': {
-                        'environment.clastix.io': 'tenant1',
-                        'tier.clastix.io': '0'
-                    },
-                    'labels': {
-                        'tenant.clastix.io': 'tenant1',
-                        'kind.clastix.io': 'service'
-                    }
-                },
-                'serviceType': 'NodePort'
-            }
-        },
-        'kubernetes': {
-            'version': 'v1.23.4',
-            'kubelet': {
-                'cgroupfs': 'cgroupfs'
-            },
-            'admissionControllers': [
-                'LimitRanger',
-                'ResourceQuota'
-            ]
-        },
-        'networkProfile': {
-            'address': '172.19.0.2',
-            'port': 31443,
-            'certSANs': [
-                'test.clastixlabs.io'
-            ],
-            'serviceCidr': '10.96.0.0/16',
-            'podCidr': '10.244.0.0/16',
-            'dnsServiceIPs': [
-                '10.96.0.10'
-            ]
-        },
-        'addons': {
-            'coreDNS': {},
-            'kubeProxy': {}
-        }
-    }
-}
-tenant_yaml = yaml.dump(tenant)
+    print(f"Cluster '{cluster_name}' created successfully.")
 
-# Use kubectl to create the CRD
+#list resources created
+    list_resources = f'kubectl get tcp,deploy,pods,svc'
+    subprocess.run(list_resources, shell=True, check=True)
+    
+    # return config
 
-subprocess.run(["kubectl", "apply", "-f", "-"], input=tenant_yaml.encode())
+#created cluster-info
+    created_cluster_info = f'kubectl --kubeconfig={config} cluster-info'
+    subprocess.run(created_cluster_info, shell=True, check=True)
 
-# retrieve kubeconfig and store in /tmp/kubeconfig
+    created_cluster_svc = f'kubectl --kubeconfig={config} get svc'
+    subprocess.run(created_cluster_svc, shell=True, check=True)
 
-config_detail = f'kubectl get secrets {secret_name} -o json | jq -r \'.data["admin.conf"]\' | base64 -d'
-subprocess.run(config_detail, shell=True, check=True)
 
-command = f'kubectl get secrets {secret_name} -o json | jq -r \'.data["admin.conf"]\' | base64 -d > {config}'
-subprocess.run(command, shell=True, check=True)
 
-#list tcp, deployment, pods and svc
-list_resources = f'kubectl get tcp,deploy,pods,svc'
-subprocess.run(list_resources, shell=True, check=True)
+def delete_cluster(cluster_name):
+    delete_command = f"kubectl delete tenantcontrolplane {cluster_name}"
+    subprocess.run(delete_command, shell=True, check=True)
 
-# config_cmd = f'kubectl --kubeconfig={config} config view'
-# subprocess.run(config_cmd, shell=True, check=True)
-# subprocess.run(["kubectl", "--kubeconfig={config}", "config", "view"])
+    print(f"Cluster '{cluster_name}' deleted successfully.")
+
+def create_node():
+
+    os.environ['KUBECONFIG'] = '/tmp/kubeconfig'
+ 
+    subprocess.run("./join-node.bash", shell=True)
+    subprocess.run(["kubectl", "get", "nodes"])
+
+if __name__ == "__main__":
+    operation = input("Enter operation (create, addnode, delete): ")
+    cluster_name = input("Enter cluster name: ")
+    
+
+
+
+    if operation == "create":
+        create_cluster(cluster_name, yaml_file_path= "kamaji-tenant.yaml")
+        # list_cluster()
+        # cluster_name = input("Enter cluster name: ")
+    elif operation == "addnode":
+        create_node()
+    # elif operation == "update":
+    #     update_cluster(cluster_name)
+    elif operation == "delete":
+        delete_cluster(cluster_name)
+    else:
+        print("Invalid operation.")
+
